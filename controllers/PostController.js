093dddf765e71127fd5e572c86e53ea87458a214
DanielMiguelez <danielmiguelez1993@gmail.com>
2025-03-08 17:00:09 +0100

    get post by id and by name

--- controllers/PostController.js
+++ controllers/PostController.js
@@ -10,6 +10,37 @@ const PostController = {
             console.error(error);
             res.status(500).send({msg:"Could not create the post", error})
         }
+    },
+
+    async getAllPosts (req, res){
+        try {
+            const posts = await Post.find()
+            res.status(200).send({msg:"Posts list", posts})
+        } catch (error) {
+            console.error(error);
+            res.status(500).send({msg:"Could not bring posts", error})
+        }
+    },
+
+    async getPostById (req, res){
+        try {
+            const post = await Post.findById(req.params._id)
+            res.status(200).send({msg:"Here is your post", post})
+        } catch (error) {
+            console.error(error);
+            res.status(500).send({msg:"Could not find your post", error})
+        }
+    },
+
+    async getPostByName (req, res){
+        try {
+            const name = new RegExp(req.params.name, "i");
+            const posts = await Post.find({name})
+            res.status(200).send({posts})
+        } catch (error) {
+            console.error(error);
+            res.status(500).send({msg:"Could not find your posts", error})
+        }
     }
 }
 
--- routes/posts.js
+++ routes/posts.js
@@ -4,6 +4,7 @@ const router = express.Router();
 const PostController = require("../controllers/PostController")
 
 router.post("/createPost", PostController.createPost)
-
+router.get("/getAllPosts", PostController.getAllPosts)
+router.get("/getPostById/:_id", PostController.getPostById)
 
 module.exports = router;
